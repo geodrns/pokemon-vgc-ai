3c5fb837df89e49d6b69cccdc1f76538b5b855ae
Simão Reis <dracostriker@hotmail.com>
2023-10-16 21:08:22 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vgc',
+    version='2.2.0',
+    description='The VGC AI Framework aims to emulate the Esports scenario of human video game championships of Pokémon with AI agents, including the game balance aspect.',
+    url='https://gitlab.com/DracoStriker/pokemon-vgc-engine',
+    author='Simão Reis',
+    author_email='simao.reis@outlook.pt',
+    license='MIT License',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['numpy>=1.15.4',
+                      'gym>=0.10.9',
+                      'PySimpleGUI>=4.20.0',
+                      'simple-plugin-loader>=1.6',
+                      'elo~=0.1.1',
+                      'arcade~=2.6.7',
+                      ],
+    classifiers=[
+        'Development Status :: 2 - Development',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.9.9',
+    ],
+)
